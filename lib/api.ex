19708e1f2e3b0232044161f79e5a888f0be6e13d
Iheanyi Ekechukwu <iekechukwu@gmail.com>
2015-12-08 23:11:44 -0600

    Move Course Section processing into it's own function. Still broken.

--- lib/api.ex
+++ lib/api.ex
@@ -56,6 +56,118 @@ defmodule API do
   end
 
   @doc """
+  Processes the HTML for a course row.
+  """
+  def process_section_html(first_section) do
+      {_, _, first_cell} = Enum.at(first_section, 0)
+      {_, _, course_num_section_text} = Enum.at(first_cell, 0)
+      {_, course_books_link_tag, _} = Enum.at(first_cell, 2)
+      
+      # Prints out the course number and the section number. O_O
+      course_num_section = List.first(course_num_section_text)
+      {_, course_books_link} = Enum.at(course_books_link_tag, 0)
+      course_books_link
+      [course_num, course_section] = String.split(course_num_section," - ", trim: true)
+ 
+      # Second Cell - Course Title
+      {_, _, second_cell} = Enum.at(first_section, 1)
+      course_title = List.first(second_cell)
+      #{_, _, course_title_text} = Enum.at(second_cell, 0)
+     
+      #course_title = List.first(course_title_text)
+      #{_, _, course_title_text} = Enum.at(second_cell, 0)
+      #course_title = List.first(course_title_text)
+
+      # Third Cell - Credits
+      {_, _, third_cell} = Enum.at(first_section, 2)
+      credits = List.first(third_cell) 
+
+      # Fourth Cell - Status
+      {_, _, fourth_cell} = Enum.at(first_section, 3)
+      status = List.first(fourth_cell)
+
+      # Fifth Cell - Max Seats
+      {_, _, fifth_cell} = Enum.at(first_section, 4)
+      max_seats = List.first(fifth_cell)
+
+      # Sixth Cell - Open Seats
+      {_, _, sixth_cell} = Enum.at(first_section, 5)
+      open_seats = List.first(sixth_cell)
+
+      # Seventh Cell - Cross Listed
+      {_, _, seventh_cell} = Enum.at(first_section, 6)
+      crosslisted = List.first(seventh_cell)
+
+      # Eighth Cell - CRN
+      {_, _, eighth_cell} = Enum.at(first_section, 7)
+      course_reg_number = String.strip(List.first(eighth_cell))
+
+      # Ninth Cell - Syllabus
+      {_, _, ninth_cell} = Enum.at(first_section, 8)
+      syllabus = List.first(ninth_cell)
+
+      # Tenth Cell - Instructor
+      # May be more than one of these, would be wise to split these on the
+      # instances of the anchor tags in this element, for real.
+      {_, _, tenth_cell} = Enum.at(first_section, 9)
+      instructor_full_name = Floki.text(tenth_cell)
+      #IO.puts instructor_full_name
+      instructor_name_array = String.split(instructor_full_name, ",", trim: true)
+      name_length = length instructor_name_array
+      IO.puts name_length
+      
+      # If name_length >= 2, we have a valid instructor
+      # In order to further refine the instructor,
+      # We can access the `href` of the <a> tag
+      # and use regex to match `P=<id>'`.
+      # Storing this in the database will allow us to know exactly
+      # which specific instructor we are looking at. So therefore,
+      # Instructors with mad common names like
+      # Mike Johnson will not be duplicated in the array.
+      if name_length >= 2 do
+        [instructor_last, instructor_first] = instructor_name_array
+        instructor = "#{instructor_first} #{instructor_last}"
+      else 
+        # Instructor is to be determined/announced, so default to that.
+        instructor = "TBA"
+      end
+      # Eleventh Cell - Timeslots
+      # *Note: May have more than one timeslot with the (1).
+      # This will probably be reflected as a one-to-many Section to Timeslots
+      # in Phoenix.
+      # Additionally, gotta split on the timeslots by their start time, end
+      # times, and days of the week.
+      # Gotta parse this and make the timeslots for real, for reals. u_u 
+      {_, _, eleventh_cell} = Enum.at(first_section, 10)
+      timeslots = String.strip(List.first(eleventh_cell))
+
+      # Tweltfhh Cell - Begin Date
+      # *Note: May have more than one begin date if they have more than one
+      # timeslot. :/ Gotta do that some magic here. Luckily, shouldn't be as
+      # difficult.
+      {_, _, twelfth_cell} = Enum.at(first_section, 11)
+      begin_date = String.strip(List.first(twelfth_cell))
+
+      # Thirteenth Cell - End Date
+      # *Note: May have more than one end date, ala Timeslots.
+      {_, _, thirteenth_cell} = Enum.at(first_section, 12)
+      end_date = String.strip(List.first(thirteenth_cell))
+
+
+      # Fourteenth Cell - Where
+      # * Note - May have more than one location, break on splits fam. 
+      {_, _, fourteenth_cell} = Enum.at(first_section, 13)
+      location = String.strip(List.first(fourteenth_cell))
+      
+      IO.puts "#{course_num} - #{course_section} - #{course_title}"
+      IO.puts "CRN #{course_reg_number}"
+      IO.puts "Taught by #{instructor} on #{timeslots}"
+      IO.puts "#{credits} credits, #{open_seats}/#{max_seats} seats left"
+      IO.puts "Starts #{begin_date} and Ends #{end_date}"
+      IO.puts "Location: #{location}"
+  end
+
+  @doc """
   Fetches the HTML for the designated term and dept
   """
   def fetch_term_dept_html(term, dept) do
@@ -63,10 +175,10 @@ defmodule API do
       html =
       HTTPoison.post!(@base_url,
       {:form , [
-          "TERM": "201600",
+          "TERM": term,
           "DIVS": "A",
           "CAMPUS": "M",
-          "SUBJ": "ACCT",
+          "SUBJ": dept,
           "ATTR": "0ANY",
           "CREDIT": "A",
         ]},
@@ -76,8 +188,10 @@ defmodule API do
       course_sections = Floki.find(html, "#resulttable tbody tr")
 
       course_sections
-      |> Enum.map(fn section -> 
+      |> Enum.map(fn {_, _, section} -> 
         #IO.puts section[1]
+        IO.puts Floki.text(section)
+        process_section_html(section)
       end
       )
 
@@ -155,8 +269,27 @@ defmodule API do
       # May be more than one of these, would be wise to split these on the
       # instances of the anchor tags in this element, for real.
       {_, _, tenth_cell} = Enum.at(first_section, 9)
-      instructor = String.strip(List.first(tenth_cell))
-
+      tenth_cell
+      instructor_full_name = Floki.text(tenth_cell)
+      instructor_name_array = String.split(instructor_full_name, ",", trim: true)
+      name_length = length instructor_name_array
+      IO.puts name_length
+      
+      # If name_length >= 2, we have a valid instructor
+      # In order to further refine the instructor,
+      # We can access the `href` of the <a> tag
+      # and use regex to match `P=<id>'`.
+      # Storing this in the database will allow us to know exactly
+      # which specific instructor we are looking at. So therefore,
+      # Instructors with mad common names like
+      # Mike Johnson will not be duplicated in the array.
+      if name_length >= 2 do
+        [instructor_last, instructor_first] = instructor_name_array
+        instructor = "#{instructor_first} #{instructor_last}"
+      else 
+        # Instructor is to be determined/announced, so default to that.
+        instructor = "TBA"
+      end
       # Eleventh Cell - Timeslots
       # *Note: May have more than one timeslot with the (1).
       # This will probably be reflected as a one-to-many Section to Timeslots
@@ -214,7 +347,12 @@ defmodule API do
     first_term = List.first(terms)
     first_dept = List.first(depts)
 
-    fetch_term_dept_html(first_term['value'], first_dept['value'])
+    first_term_value = "201520" #first_term['value']
+    first_dept_value = "AL" # first_dept['value']
+
+    IO.puts "Fetching Term Department stuff"
+    IO.puts(first_term_value)
+    fetch_term_dept_html(first_term_value, first_dept_value)
   end
 end
 
