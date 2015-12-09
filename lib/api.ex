822042402cf4f0c6a5cc8d83c8a41fea6ca378e7
Iheanyi Ekechukwu <iekechukwu@gmail.com>
2015-12-08 22:29:22 -0600

    Print out every single detail about courses, prepare for future insertion into a database.

--- lib/api.ex
+++ lib/api.ex
@@ -77,7 +77,6 @@ defmodule API do
 
       course_sections
       |> Enum.map(fn section -> 
-        IO.puts Floki.text(section)
         #IO.puts section[1]
       end
       )
@@ -101,8 +100,10 @@ defmodule API do
       # (11) Begin/Start date for the course.
       # (12) End date for the course.
       # (13) Course Location.
+      # Also, probably should insert an actual link/reference to the actual
+      # course description page...JUST IN CASE, feel me?
 
-      # First Cell
+      # First Cell - Course Number and Section Number
       {tag, attrs, first_section} = Enum.at(course_sections, 0)
       {_, _, first_cell} = Enum.at(first_section, 0)
       {_, _, course_num_section_text} = Enum.at(first_cell, 0)
@@ -113,14 +114,81 @@ defmodule API do
       course_books_link
       [course_num, course_section] = String.split(course_num_section," - ", trim: true)
  
-      # Second Cell
-      {_, _, second_section} = Enum.at(course_sections, 1)
-      {_, _, second_cell} = Enum.at(second_section, 1)
+      # Second Cell - Course Title
+      {_, _, second_cell} = Enum.at(first_section, 1)
       course_title = List.first(second_cell)
+      #{_, _, course_title_text} = Enum.at(second_cell, 0)
+     
+      #course_title = List.first(course_title_text)
       #{_, _, course_title_text} = Enum.at(second_cell, 0)
       #course_title = List.first(course_title_text)
 
+      # Third Cell - Credits
+      {_, _, third_cell} = Enum.at(first_section, 2)
+      credits = List.first(third_cell) 
+
+      # Fourth Cell - Status
+      {_, _, fourth_cell} = Enum.at(first_section, 3)
+      status = List.first(fourth_cell)
+
+      # Fifth Cell - Max Seats
+      {_, _, fifth_cell} = Enum.at(first_section, 4)
+      max_seats = List.first(fifth_cell)
+
+      # Sixth Cell - Open Seats
+      {_, _, sixth_cell} = Enum.at(first_section, 5)
+      open_seats = List.first(sixth_cell)
+
+      # Seventh Cell - Cross Listed
+      {_, _, seventh_cell} = Enum.at(first_section, 6)
+      crosslisted = List.first(seventh_cell)
+
+      # Eighth Cell - CRN
+      {_, _, eighth_cell} = Enum.at(first_section, 7)
+      course_reg_number = String.strip(List.first(eighth_cell))
+
+      # Ninth Cell - Syllabus
+      {_, _, ninth_cell} = Enum.at(first_section, 8)
+      syllabus = List.first(ninth_cell)
+
+      # Tenth Cell - Instructor
+      # May be more than one of these, would be wise to split these on the
+      # instances of the anchor tags in this element, for real.
+      {_, _, tenth_cell} = Enum.at(first_section, 9)
+      instructor = String.strip(List.first(tenth_cell))
+
+      # Eleventh Cell - Timeslots
+      # *Note: May have more than one timeslot with the (1).
+      # This will probably be reflected as a one-to-many Section to Timeslots
+      # in Phoenix.
+      # Gotta parse this and make the timeslots for real, for reals. u_u 
+      {_, _, eleventh_cell} = Enum.at(first_section, 10)
+      timeslots = String.strip(List.first(eleventh_cell))
+
+      # Tweltfhh Cell - Begin Date
+      # *Note: May have more than one begin date if they have more than one
+      # timeslot. :/ Gotta do that some magic here. Luckily, shouldn't be as
+      # difficult.
+      {_, _, twelfth_cell} = Enum.at(first_section, 11)
+      begin_date = String.strip(List.first(twelfth_cell))
+
+      # Thirteenth Cell - End Date
+      # *Note: May have more than one end date, ala Timeslots.
+      {_, _, thirteenth_cell} = Enum.at(first_section, 12)
+      end_date = String.strip(List.first(thirteenth_cell))
+
+
+      # Fourteenth Cell - Where
+      # * Note - May have more than one location, break on splits fam. 
+      {_, _, fourteenth_cell} = Enum.at(first_section, 13)
+      location = String.strip(List.first(fourteenth_cell))
+      
       IO.puts "#{course_num} - #{course_section} - #{course_title}"
+      IO.puts "CRN #{course_reg_number}"
+      IO.puts "Taught by #{instructor} on #{timeslots}"
+      IO.puts "#{credits} credits, #{open_seats}/#{max_seats} seats left"
+      IO.puts "Starts #{begin_date} and Ends #{end_date}"
+      IO.puts "Location: #{location}"
   end
 
   @doc """
