23ad3d7b248a14affd005761582571634a728538
Iheanyi Ekechukwu <iekechukwu@gmail.com>
2015-12-12 16:37:44 -0600

    Add support for multiple instructors. \o/ Also, extraction of book links. ;)

--- lib/api.ex
+++ lib/api.ex
@@ -59,14 +59,35 @@ defmodule API do
   Processes the HTML for a course row.
   """
   def process_section_html(first_section) do
-      {_, _, first_cell} = Enum.at(first_section, 0)
-      {_, _, course_num_section_text} = Enum.at(first_cell, 0)
-      {_, course_books_link_tag, _} = Enum.at(first_cell, 2)
-      
+      # Each Cell/Index
+      # (0) Course Section and Course Number, also has URL link to the books
+      # relevant to that section in Hammes Bookstore.
+      # (1) Title of the course.
+      # (2) Number of credits for the course.
+      # (3) Status of the course seats (OP for open, CL for closed).
+      # (4) Max number of seats.
+      # (5) Open number of seats.
+      # (6) Cross-Listed?
+      # (7) CRN for the course. 
+      # (8) Syllabus for the course
+      # (9) Instructor for the course!
+      # (10) When the course meets, course start time.
+      # * Note, these may be prone to having more than one start time, so we
+      # have to think of how to show / reflect this in the user interface for
+      # various sub-sections. Design flaw on ND's part. -_- 
+      # (11) Begin/Start date for the course.
+      # (12) End date for the course.
+      # (13) Course Location.
+      # Also, probably should insert an actual link/reference to the actual
+      # course description page...JUST IN CASE, feel me?
+      {_, _, first_cell} = List.first(first_section)
+      {_, _, course_num_section_text} = List.first(first_cell)
+      # The books link will always be the last element.
+      {_, course_books_link_tag, _} = List.last(first_cell)
+     
       # Prints out the course number and the section number. O_O
       course_num_section = List.first(course_num_section_text)
-      {_, course_books_link} = Enum.at(course_books_link_tag, 0)
-      course_books_link
+      {_, course_books_link} = List.first(course_books_link_tag)
       [course_num, course_section] = String.split(course_num_section," - ", trim: true)
  
       # Second Cell - Course Title
@@ -110,11 +131,26 @@ defmodule API do
       # May be more than one of these, would be wise to split these on the
       # instances of the anchor tags in this element, for real.
       {_, _, tenth_cell} = Enum.at(first_section, 9)
-      instructor_full_name = Floki.text(tenth_cell)
-      #IO.puts instructor_full_name
-      instructor_name_array = String.split(instructor_full_name, ",", trim: true)
-      name_length = length instructor_name_array
-      IO.puts name_length
+      
+      # Define instructors as an array
+      instructors = Enum.map(tenth_cell, fn tag -> 
+        IO.puts "In the map"
+        IO.puts (is_tuple tag)
+        if (is_tuple tag) do 
+          # We know we have links in this.
+          instructor_full_name = Floki.text(tag)
+          {html_tag, html_attributes, html_text} = tag
+          instructor_name_text = List.first(html_text)
+          instructor_name_array = String.split(instructor_name_text, ", ", trim: true)
+          [instructor_last, instructor_first] = instructor_name_array
+          instructor = "#{instructor_first} #{instructor_last}"
+        else 
+          # The instructor will probably be TBA -_-
+          instructor = "TBA"
+        end
+        IO.puts instructor 
+        instructor
+      end)
       
       # If name_length >= 2, we have a valid instructor
       # In order to further refine the instructor,
@@ -124,13 +160,7 @@ defmodule API do
       # which specific instructor we are looking at. So therefore,
       # Instructors with mad common names like
       # Mike Johnson will not be duplicated in the array.
-      if name_length >= 2 do
-        [instructor_last, instructor_first] = instructor_name_array
-        instructor = "#{instructor_first} #{instructor_last}"
-      else 
-        # Instructor is to be determined/announced, so default to that.
-        instructor = "TBA"
-      end
+      
       # Eleventh Cell - Timeslots
       # *Note: May have more than one timeslot with the (1).
       # This will probably be reflected as a one-to-many Section to Timeslots
@@ -161,10 +191,12 @@ defmodule API do
       
       IO.puts "#{course_num} - #{course_section} - #{course_title}"
       IO.puts "CRN #{course_reg_number}"
-      IO.puts "Taught by #{instructor} on #{timeslots}"
+      IO.puts "Timeslots: #{timeslots}"
+      IO.puts "Instructor(s): #{Enum.join(instructors, ", ")}"
       IO.puts "#{credits} credits, #{open_seats}/#{max_seats} seats left"
       IO.puts "Starts #{begin_date} and Ends #{end_date}"
       IO.puts "Location: #{location}"
+      IO.puts "Books Link: #{course_books_link}"
   end
 
   @doc """
@@ -189,141 +221,10 @@ defmodule API do
 
       course_sections
       |> Enum.map(fn {_, _, section} -> 
-        #IO.puts section[1]
-        IO.puts Floki.text(section)
         process_section_html(section)
       end
       )
 
-      # Each Cell/Index
-      # (0) Course Section and Course Number, also has URL link to the books
-      # relevant to that section in Hammes Bookstore.
-      # (1) Title of the course.
-      # (2) Number of credits for the course.
-      # (3) Status of the course seats (OP for open, CL for closed).
-      # (4) Max number of seats.
-      # (5) Open number of seats.
-      # (6) Cross-Listed?
-      # (7) CRN for the course. 
-      # (8) Syllabus for the course
-      # (9) Instructor for the course!
-      # (10) When the course meets, course start time.
-      # * Note, these may be prone to having more than one start time, so we
-      # have to think of how to show / reflect this in the user interface for
-      # various sub-sections. Design flaw on ND's part. -_- 
-      # (11) Begin/Start date for the course.
-      # (12) End date for the course.
-      # (13) Course Location.
-      # Also, probably should insert an actual link/reference to the actual
-      # course description page...JUST IN CASE, feel me?
-
-      # First Cell - Course Number and Section Number
-      {tag, attrs, first_section} = Enum.at(course_sections, 0)
-      {_, _, first_cell} = Enum.at(first_section, 0)
-      {_, _, course_num_section_text} = Enum.at(first_cell, 0)
-      {_, course_books_link_tag, _} = Enum.at(first_cell, 2)
-      # Prints out the course number and the section number. O_O
-      course_num_section = List.first(course_num_section_text)
-      {_, course_books_link} = Enum.at(course_books_link_tag, 0)
-      course_books_link
-      [course_num, course_section] = String.split(course_num_section," - ", trim: true)
- 
-      # Second Cell - Course Title
-      {_, _, second_cell} = Enum.at(first_section, 1)
-      course_title = List.first(second_cell)
-      #{_, _, course_title_text} = Enum.at(second_cell, 0)
-     
-      #course_title = List.first(course_title_text)
-      #{_, _, course_title_text} = Enum.at(second_cell, 0)
-      #course_title = List.first(course_title_text)
-
-      # Third Cell - Credits
-      {_, _, third_cell} = Enum.at(first_section, 2)
-      credits = List.first(third_cell) 
-
-      # Fourth Cell - Status
-      {_, _, fourth_cell} = Enum.at(first_section, 3)
-      status = List.first(fourth_cell)
-
-      # Fifth Cell - Max Seats
-      {_, _, fifth_cell} = Enum.at(first_section, 4)
-      max_seats = List.first(fifth_cell)
-
-      # Sixth Cell - Open Seats
-      {_, _, sixth_cell} = Enum.at(first_section, 5)
-      open_seats = List.first(sixth_cell)
-
-      # Seventh Cell - Cross Listed
-      {_, _, seventh_cell} = Enum.at(first_section, 6)
-      crosslisted = List.first(seventh_cell)
-
-      # Eighth Cell - CRN
-      {_, _, eighth_cell} = Enum.at(first_section, 7)
-      course_reg_number = String.strip(List.first(eighth_cell))
-
-      # Ninth Cell - Syllabus
-      {_, _, ninth_cell} = Enum.at(first_section, 8)
-      syllabus = List.first(ninth_cell)
-
-      # Tenth Cell - Instructor
-      # May be more than one of these, would be wise to split these on the
-      # instances of the anchor tags in this element, for real.
-      {_, _, tenth_cell} = Enum.at(first_section, 9)
-      tenth_cell
-      instructor_full_name = Floki.text(tenth_cell)
-      instructor_name_array = String.split(instructor_full_name, ",", trim: true)
-      name_length = length instructor_name_array
-      IO.puts name_length
-      
-      # If name_length >= 2, we have a valid instructor
-      # In order to further refine the instructor,
-      # We can access the `href` of the <a> tag
-      # and use regex to match `P=<id>'`.
-      # Storing this in the database will allow us to know exactly
-      # which specific instructor we are looking at. So therefore,
-      # Instructors with mad common names like
-      # Mike Johnson will not be duplicated in the array.
-      if name_length >= 2 do
-        [instructor_last, instructor_first] = instructor_name_array
-        instructor = "#{instructor_first} #{instructor_last}"
-      else 
-        # Instructor is to be determined/announced, so default to that.
-        instructor = "TBA"
-      end
-      # Eleventh Cell - Timeslots
-      # *Note: May have more than one timeslot with the (1).
-      # This will probably be reflected as a one-to-many Section to Timeslots
-      # in Phoenix.
-      # Additionally, gotta split on the timeslots by their start time, end
-      # times, and days of the week.
-      # Gotta parse this and make the timeslots for real, for reals. u_u 
-      {_, _, eleventh_cell} = Enum.at(first_section, 10)
-      timeslots = String.strip(List.first(eleventh_cell))
-
-      # Tweltfhh Cell - Begin Date
-      # *Note: May have more than one begin date if they have more than one
-      # timeslot. :/ Gotta do that some magic here. Luckily, shouldn't be as
-      # difficult.
-      {_, _, twelfth_cell} = Enum.at(first_section, 11)
-      begin_date = String.strip(List.first(twelfth_cell))
-
-      # Thirteenth Cell - End Date
-      # *Note: May have more than one end date, ala Timeslots.
-      {_, _, thirteenth_cell} = Enum.at(first_section, 12)
-      end_date = String.strip(List.first(thirteenth_cell))
-
-
-      # Fourteenth Cell - Where
-      # * Note - May have more than one location, break on splits fam. 
-      {_, _, fourteenth_cell} = Enum.at(first_section, 13)
-      location = String.strip(List.first(fourteenth_cell))
-      
-      IO.puts "#{course_num} - #{course_section} - #{course_title}"
-      IO.puts "CRN #{course_reg_number}"
-      IO.puts "Taught by #{instructor} on #{timeslots}"
-      IO.puts "#{credits} credits, #{open_seats}/#{max_seats} seats left"
-      IO.puts "Starts #{begin_date} and Ends #{end_date}"
-      IO.puts "Location: #{location}"
   end
 
   @doc """
@@ -333,13 +234,21 @@ defmodule API do
     terms = fetch_terms
     depts = fetch_departments
 
-    Enum.each(terms, fn term -> 
-      IO.puts term.value
-      Enum.each(depts, fn dept -> 
-        fetch_term_dept_html(term.value, dept.value)
-      end)
-    end
-    )
+    first_term = terms
+    |> List.first
+    
+    IO.puts first_term.name
+    Enum.each(depts, fn dept -> 
+      fetch_term_dept_html(first_term.value, dept.value)
+    end)
+
+
+    #Enum.each(terms, fn term -> 
+    #  IO.puts term.value
+    #  Enum.each(depts, fn dept -> 
+    #    fetch_term_dept_html(term.value, dept.value)
+    #  end)
+    #end)
   end
 
   def process_course_html(html) do 
